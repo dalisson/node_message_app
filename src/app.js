3cb544c7812182a814daecb1280d41e3d833609d
dalissonfigueiredo <dalissonfigueiredo@gmail.com>
2021-09-19 09:35:11 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+
+// app.use((req, res, next)=>{
+
+//     res.status(503).send("server under maintencance")
+// })
+
+
+module.exports = app
